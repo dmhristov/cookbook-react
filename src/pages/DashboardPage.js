426ac65a7a8342ed76ccd662dcec82a0054928cd
Denislav Hristov <denislav.hristov@dxc.com>
2022-11-07 17:52:35 +0200

    Add base fetch functionality

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,12 +1,26 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Stack from "react-bootstrap/Stack";
 import DashboardNav from "../components/DashboardNav";
 import RecipeCard from "../components/RecipeCard";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const DashboardPage = () => {
-    const [recipes, setRecipes] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+    const [recipes, setRecipes] = useState([]);
+    const recipesCollectionRef = collection(db, "recipes");
+
+    useEffect(() => {
+        const getRecipes = async () => {
+            const response = await getDocs(recipesCollectionRef);
+            const data = response.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() };
+            });
+            setRecipes(data);
+        };
+        getRecipes();
+    }, []);
 
     return (
         <Stack gap={4} className="d-flex flex-column">
@@ -15,7 +29,13 @@ const DashboardPage = () => {
             <Container className="flex-grow-1 m-auto">
                 <Row className="gap-2">
                     {recipes.map((r) => (
-                        <RecipeCard key={r}></RecipeCard>
+                        <RecipeCard
+                            key={r.id}
+                            imageUrl={r.imageUrl}
+                            title={r.title}
+                            fromDate={r.date.toDate().toLocaleDateString()}
+                            authorId={r.authorId}
+                        ></RecipeCard>
                     ))}
                 </Row>
             </Container>
