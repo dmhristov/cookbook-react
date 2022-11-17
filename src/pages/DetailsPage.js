56cbe64c0bef80d660aa0b88549352aec4b01717
Denislav Hristov <denislav.hristov@dxc.com>
2022-11-17 16:54:32 +0200

    Restructure details page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
-import loginImage from "../assets/images/login_image.jpg";
 import Comments from "../components/Comments";
 import { Link, useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
@@ -17,7 +16,7 @@ const DetailsPage = () => {
         const getRecipeById = async () => {
             const recipeRef = doc(db, "recipes", recipeId);
             const recipeData = await getDoc(recipeRef);
-            
+
             if (!recipeData.exists()) {
                 setLoading(false);
                 return setPageNotFound(true);
@@ -25,9 +24,11 @@ const DetailsPage = () => {
 
             const authorRef = doc(db, "users", recipeData.data().authorId);
             const authorData = await getDoc(authorRef);
-            
+
             setRecipe({
-                authorNames: `${authorData.data().firstName} ${authorData.data().lastName}`,
+                authorNames: `${authorData.data().firstName} ${
+                    authorData.data().lastName
+                }`,
                 ...recipeData.data(),
             });
             setLoading(false);
@@ -37,43 +38,64 @@ const DetailsPage = () => {
     }, []);
 
     // TODO -> add page not found
-    return ( !loading &&
-        <Container className="d-flex align-items-center align-self-start mt-5">
-            
-            <Stack
-                gap={2}
-                className="d-flex flex-column align-self-start justify-content-center"
-                style={{ maxWidth: "52rem" }}
-            >
-                <img
-                    className="rounded mb-2"
-                    style={{ width: "48rem", height: "28rem" }}
-                    src={recipe.imageUrl}
-                    alt="recipe"
-                ></img>
-                <Comments />
-            </Stack>
-            <Stack gap={2} className="ms-2" style={{ maxWidth: "26rem" }}>
-                <div>
-                    <h2 className="mb-3">{recipe.title}</h2>
-                    <p>Author Name: <Link className="fw-bold text-decoration-none text-primary" to="/">{recipe.authorNames}</Link></p>
-                    <p>From: {recipe.date.toDate().toLocaleDateString()}</p>
-                    {/* TODO -> refactor db and implement likes */}
-                    <p>
-                        Likes:
-                        {}
-                    </p>
-                </div>
-                <div>
-                    <h3 className="mb-3">Ingredients:</h3>
-                    <p style={{whiteSpace: "pre-wrap"}}>{recipe.ingredients}</p>
-                </div>
-                <div>
-                    <h3 className="mb-3">Description</h3>
-                    <p style={{whiteSpace: "pre-wrap"}}>{recipe.description}</p>
-                </div>
-            </Stack>
-        </Container>
+    return (
+        !loading && (
+            <Container className="d-flex align-items-center align-self-start mt-3 flex-column">
+                <Stack
+                    gap={2}
+                    className="d-flex flex-column align-self-center justify-content-center"
+                >
+                    <img
+                        className="rounded mb-2"
+                        src={recipe.imageUrl}
+                        alt="recipe"
+                    ></img>
+                    <Container className="d-flex justify-content-between">
+                        <div>
+                            <h2 className="mb-3">{recipe.title}</h2>
+                            <p>
+                                Author Name:{" "}
+                                <Link
+                                    className="fw-bold text-decoration-none text-success"
+                                    to="/"
+                                >
+                                    {recipe.authorNames}
+                                </Link>
+                            </p>
+                            <p className="text-muted">
+                                From:{" "}
+                                {recipe.date.toDate().toLocaleDateString()}
+                            </p>
+                           
+                        </div>
+                         {/* TODO -> implement likes */}
+                        <p>
+                            Likes:
+                            {}
+                        </p>
+                    </Container>
+                    <hr className="my-2 border-3 border-top border-success"></hr>
+                    <Container className="d-flex justify-content-between">
+                        
+                        <div className="" style={{maxWidth: "48rem"}}>
+                            <h3 className="me-3 mb-3">Description:</h3>
+                            <p className="mb-5" style={{ whiteSpace: "pre-wrap" }}>
+                                {recipe.description}
+                            </p>
+                            
+                            <Comments />
+                        </div>
+                        <div className="justify-self-end">
+                            <h3 className="mb-3">Ingredients:</h3>
+                            <p style={{ whiteSpace: "pre-wrap" }}>
+                                {recipe.ingredients}
+                            </p>
+                        </div>
+                    </Container>
+                </Stack>
+                
+            </Container>
+        )
     );
 };
 
